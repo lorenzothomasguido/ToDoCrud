77ad671e7e0a7b840aca297349c469dd9daa0cf7
lorenzo <lorenzothomasguido@gmail.com>
2023-12-22 15:52:09 -0300

    save04

--- src/main/java/com/Todo/app/rest/Model/Task.java
+++ src/main/java/com/Todo/app/rest/Model/Task.java
@@ -1,17 +1,14 @@
 package com.Todo.app.rest.Model;
 
-<<<<<<< HEAD
 //import jakarta.persistence.*;
 import lombok.*;
 
 import javax.persistence.*;
 
 
-=======
 import lombok.*;
 
 import javax.persistence.*;
->>>>>>> f20b1e4cb721c6cff060426822a22307d615dc02
 @Data
 @Entity
 @NoArgsConstructor
